b450e63d1c8a417d369075725336180f69f89426
bharathiram <bharathi.ram@engrade.com>
2018-06-03 18:07:21 -0700

    Added dependenct packages & initial build modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
   res.send("Maybe what you are is what's trapping you inside whatever it is that you're trapped inside.!")
-})
-app.listen(3000, () => console.log('Server running on port 3000'))
+});
+app.listen(3000, () => console.log('Server running on port 3000'));
